243c14410ef888127f2a100d834d04bfd960e6d6
Alexander <zheiling@hotmail.com>
2019-09-05 00:24:13 +0300

    Fetch API. First step

--- src/App.js
+++ src/App.js
@@ -3,45 +3,47 @@ import "./App.css";
 // import UIkit from 'uikit';
 // import Icons from 'uikit/dist/js/uikit-icons';
 
-//TODO
-
-// * Apply styles
-
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+
+  }
+
+  componentDidMount() {
+
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
+
+  }
+
+  setSearchTopStories(result) {
+      this.setState({ result });
   }
 
   onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const isNotId = item => item.objectID !== id,
+          updatedHits = this.state.result.hits.filter(isNotId);
+
+    this.setState({ 
+        result : { ...this.state.result, hits: updatedHits }
+     });
    }
 
    onSearchChange(event) {
@@ -49,7 +51,8 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list} = this.state;
+    const { searchTerm, result} = this.state;
+    
     return (
       <div className="App">
         <h1 className="uk-heading-small uk-text-center">Books List</h1>
@@ -61,11 +64,13 @@ class App extends Component {
             ukIcon="icon: search"
           >
           </Search>
-          <Table 
-            list={list}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+          { result &&
+            <Table 
+              list={result.hits}
+              pattern={searchTerm}
+              onDismiss={this.onDismiss}
+            /> 
+          }
         </div>
       </div>
     );
